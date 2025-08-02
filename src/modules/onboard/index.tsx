9a797aef710373a7563629ae9b45a3fa87c34ce2
manish.kumar <kumar.manish@medikabazaar.com>
2025-08-02 19:41:59 +0530

    splash

--- src/modules/onboard/index.tsx
+++ src/modules/onboard/index.tsx
@@ -1,14 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Colors, screenHeight, screenWidth } from '@utils/Constants';
+import Animated, { FadeInDown } from 'react-native-reanimated';
+import CustomText from '@components/global/CustomText';
+import { resetAndNavigate } from '@utils/NavigationUtil';
 
 const Splash = () => {
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      resetAndNavigate('Home');
+    }, 3000);
+    return () => clearTimeout(timeoutId);
+  }, []);
   return (
-    <View>
-      <Text>Splash</Text>
+    <View style={styles.container}>
+      <Image
+        source={require('@assets/images/logo_t.png')}
+        style={styles.image}
+      ></Image>
     </View>
   );
 };
 
 export default Splash;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary,
+  },
+  image: {
+    width: screenWidth * 0.35,
+    height: screenWidth * 0.35,
+    resizeMode: 'contain',
+  },
+  msgText: {
+    textAlign: 'center',
+  },
+  treeImage: {
+    width: screenWidth * 0.4,
+    height: screenHeight * 0.14,
+    resizeMode: 'contain',
+  },
+  animatedContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 40,
+  },
+});
